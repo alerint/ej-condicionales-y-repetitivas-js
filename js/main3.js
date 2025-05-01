03499bd0e605e13399f65f8286ed09b40bcda488
Alejandra Rintflaich <alejandrarintflaich@gmail.com>
2025-05-01 14:26:44 -0300

    optimización ej.c

--- js/main3.js
+++ js/main3.js
@@ -3,13 +3,13 @@ console.log('\nEjercicio c:')
 
 var cantidadDeGatos = 10
 var cantidadDePasos = 4
+var icono2 = ''
+
+for(var paso = 1; paso <= cantidadDePasos; paso++) {
+    icono2 += '🐾'
+}
 
 for(var gato = 1; gato <= cantidadDeGatos; gato++) {
-    var icono1 = (gato %2 === 0 ? '🐈 ⬛ ': '🐈')
-    var icono2 = ''
-    
-    for(var paso = 1; paso <= cantidadDePasos; paso++) {
-        icono2 += '🐾'
-    }
+    var icono1 = (gato %2 === 0 ? '🐈 ⬛ ': '🐈')   
     console.log('Gato #' + gato + ':' + ' ' + icono1 + icono2)
 }
